43fdd368ce9a122f00f723a4f450ae5d16c0ead7
Jeff-Adler <Jeffm.adler@gmail.com>
2021-06-16 22:43:25 -0400

    UserSearch completed

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 const users = [
   { name: 'John Doe', age: 20 },
@@ -7,12 +7,27 @@ const users = [
 
 //need to build this out
 export const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name.toLowerCase().includes(name.toLowerCase());
+    });
+
+    setUser(foundUser);
+  };
+
   return (
     <div>
       User Search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Find User</button>
+      <div>{user && user.name}</div>
+      <div>{user ? user.name : ''}</div>
       <ul>
         {users.map((user) => (
-          <li>
+          <li key={user.name}>
             {user.name} : {user.age}
           </li>
         ))}

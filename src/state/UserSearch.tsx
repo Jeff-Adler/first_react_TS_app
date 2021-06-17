84484f8df9b8453b2b8792cd43a48c9c053325ab
Jeff-Adler <Jeffm.adler@gmail.com>
2021-06-16 22:05:53 -0400

    added guests li render

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -1,6 +1,22 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+
+const users = [
+  { name: 'John Doe', age: 20 },
+  { name: 'Chris Adler', age: 30 },
+];
 
 //need to build this out
 export const UserSearch: React.FC = () => {
-  return <div>User Search</div>
+  return (
+    <div>
+      User Search
+      <ul>
+        {users.map((user) => (
+          <li>
+            {user.name} : {user.age}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
